92d9c32b570b62d63a55e4c29f8520e3f0d63770
youngentry <90388461+youngentry@users.noreply.github.com>
2021-10-18 18:00:58 +0900

    padstart(n,'str')

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("button#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0"); // .padstart(n, 'str') n의 자리로 만들고 그 자리에 str을 채우기
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
